692f1c464210a9018d798ebc009f9a5884ab6f3b
AgnihotriShivam <shivamagnihotri82@gmail.com>
2023-07-13 13:47:01 +0530

    This is my second commit

--- retrieve_pull_request_summary.sh
+++ retrieve_pull_request_summary.sh
@@ -5,7 +5,7 @@ read -p "Enter the repository owner: " REPO_OWNER
 read -p "Enter the repository name: " REPO_NAME
 
 # Date range for the last week
-START_DATE=$(date -v -7d +%Y-%m-%d)
+START_DATE=$(date -d "1 week ago" +%Y-%m-%d)
 END_DATE=$(date +%Y-%m-%d)
 
 # GitHub API endpoint for pull requests
@@ -22,14 +22,14 @@ in_progress=0
 for pr in $(echo "$pull_requests" | jq -r '.[] | @base64'); do
   pr_info=$(echo "$pr" | base64 --decode)
   
-  pr_created=$(echo "$pr_info" | jq -r '.created_at | strptime("%Y-%m-%dT%H:%M:%SZ") | mktime')
-  if (( pr_created < $(date -j -f "%Y-%m-%d" $START_DATE +%s) )); then
+  pr_created=$(echo "$pr_info" | jq -r '.created_at | sub("Z$"; "") | strptime("%Y-%m-%dT%H:%M:%S") | mktime')
+  if (( pr_created < $(date -d $START_DATE +%s) )); then
     break
   fi
   
   pr_state=$(echo "$pr_info" | jq -r '.state')
   
-  if (( pr_created >= $(date -j -f "%Y-%m-%d" $START_DATE +%s) )); then
+  if (( pr_created >= $(date -d $START_DATE +%s) )); then
     case $pr_state in
       "open")
         ((opened++))
@@ -49,4 +49,3 @@ echo "--------------------------------------------------"
 echo "Opened: $opened"
 echo "Closed: $closed"
 echo "In Progress: $in_progress"
-
