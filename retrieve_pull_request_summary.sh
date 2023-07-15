bf683d149b95ab4917763b09ab6c4803f1374da5
AgnihotriShivam <shivamagnihotri82@gmail.com>
2023-07-15 16:07:22 +0530

    Real tested code

--- retrieve_pull_request_summary.sh
+++ retrieve_pull_request_summary.sh
@@ -4,30 +4,36 @@
 read -p "Enter the repository owner: " REPO_OWNER
 read -p "Enter the repository name: " REPO_NAME
 
+# Calculate the date one week ago
+ONE_WEEK_AGO=$(date -d "7 days ago" +%Y-%m-%d)
+
 # Make API request to retrieve pull requests
 response=$(curl -s "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/pulls")
 
-# Extract summary details from the API response
-pr_count=$(echo "$response" | jq length)
-open_count=$(echo "$response" | jq '[.[] | select(.state == "open")] | length')
-closed_count=$(echo "$response" | jq '[.[] | select(.state == "close")] | length')
-in_progress_count=$(echo "$response" | jq '[.[] | select(.state == "open" and .draft == true)] | length')
+echo $response > response.json
 
-echo "Pull Requests Summary for $REPO_OWNER/$REPO_NAME:"
-echo "---------------------------------------------"
+# Filter pull requests created within the last week
+filtered_response=$(echo "$response" | jq --arg ONE_WEEK_AGO "$ONE_WEEK_AGO" '. | map(select(.created_at >= $ONE_WEEK_AGO))')
+
+echo $filtered_response > filtered_response.json
+
+# Extract summary details from the filtered API response
+pr_count=$(echo "$filtered_response" | jq length)
+closed_count=0
+in_progress_count=0
+
+echo "Pull Requests Summary for $REPO_OWNER/$REPO_NAME (Last Week):"
+echo "----------------------------------------------------------"
 echo "Total Pull Requests: $pr_count"
-echo "Open Pull Requests: $open_count"
-echo "Closed Pull Requests: $closed_count"
-echo "In-Progress Pull Requests: $in_progress_count"
 
-# Loop through the pull requests and print their details
+# Loop through the filtered pull requests and print their details
 for ((i = 0; i < pr_count; i++)); do
-  pr_number=$(echo "$response" | jq -r ".[$i].number")
-  pr_title=$(echo "$response" | jq -r ".[$i].title")
-  pr_author=$(echo "$response" | jq -r ".[$i].user.login")
-  pr_state=$(echo "$response" | jq -r ".[$i].state")
-  pr_created=$(echo "$response" | jq -r ".[$i].created_at")
-  pr_updated=$(echo "$response" | jq -r ".[$i].updated_at")
+  pr_number=$(echo "$filtered_response" | jq -r ".[$i].number")
+  pr_title=$(echo "$filtered_response" | jq -r ".[$i].title")
+  pr_author=$(echo "$filtered_response" | jq -r ".[$i].user.login")
+  pr_state=$(echo "$filtered_response" | jq -r ".[$i].state")
+  pr_created=$(echo "$filtered_response" | jq -r ".[$i].created_at")
+  pr_updated=$(echo "$filtered_response" | jq -r ".[$i].updated_at")
 
   echo
   echo "Pull Request #$pr_number"
@@ -36,4 +42,17 @@ for ((i = 0; i < pr_count; i++)); do
   echo "State: $pr_state"
   echo "Created: $pr_created"
   echo "Updated: $pr_updated"
+
+  # Count closed and in-progress pull requests
+  if [[ "$pr_state" == "closed" ]]; then
+    ((closed_count++))
+  elif [[ "$pr_state" == "open" ]]; then
+    ((in_progress_count++))
+  fi
 done
+
+echo
+echo "Summary:"
+echo "----------------------------------------------------------"
+echo "Closed Pull Requests: $closed_count"
+echo "Pull Requests In Progress: $in_progress_count"
