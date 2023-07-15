782f14b7e826b83fdd27d9c977be6964ac7933d1
AgnihotriShivam <shivamagnihotri82@gmail.com>
2023-07-15 09:12:08 +0530

    Creating the PR for the master branch

--- retrieve_pull_request_summary.sh
+++ retrieve_pull_request_summary.sh
@@ -6,13 +6,19 @@ read -p "Enter the repository name: " REPO_NAME
 
 # Make API request to retrieve pull requests
 response=$(curl -s "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/pulls")
-echo $response
 
 # Extract summary details from the API response
 pr_count=$(echo "$response" | jq length)
+open_count=$(echo "$response" | jq '[.[] | select(.state == "open")] | length')
+closed_count=$(echo "$response" | jq '[.[] | select(.state == "close")] | length')
+in_progress_count=$(echo "$response" | jq '[.[] | select(.state == "open" and .draft == true)] | length')
+
 echo "Pull Requests Summary for $REPO_OWNER/$REPO_NAME:"
 echo "---------------------------------------------"
 echo "Total Pull Requests: $pr_count"
+echo "Open Pull Requests: $open_count"
+echo "Closed Pull Requests: $closed_count"
+echo "In-Progress Pull Requests: $in_progress_count"
 
 # Loop through the pull requests and print their details
 for ((i = 0; i < pr_count; i++)); do
